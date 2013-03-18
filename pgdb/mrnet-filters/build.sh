d8e0878ec49bb071e43f2de93275763f9c355162
Nikoli Dryden <ndryden@xenosgame.com>
2013-03-17 22:23:11 -0500

    Actually committed updated file.

--- pgdb/mrnet-filters/build.sh
+++ pgdb/mrnet-filters/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -O0 -g -I/collab/usr/global/tools/mrnet/chaos_5_x86_64_ib/mrnet-4.0.0/include -L/collab/usr/global/tools/mrnet/chaos_5_x86_64_ib/mrnet-4.0.0/lib -lmrnet -lxplat -lpython2.6 -Wl,-rpath=/collab/usr/global/tools/mrnet/chaos_5_x86_64_ib/mrnet-4.0.0/lib -fPIC -shared -rdynamic -o test.so test.cc
+g++ -Wall -Wextra -O0 -g -I/home/ndryden/include -L/home/ndryden/lib -Wl,-rpath=/home/ndryden/lib -fPIC -shared -rdynamic -o test.so test.cc -lmrnet -lxplat -lpython2.7
