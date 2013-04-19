c8c820532b1d23348f1d0a8982000780f4b26bf5
Nikoli Dryden <ndryden@xenosgame.com>
2013-04-19 14:05:59 -0500

    Added code to filter hook for extracting times when present.

--- pgdb/mrnet-filters/filter_hook.py
+++ pgdb/mrnet-filters/filter_hook.py
@@ -11,9 +11,16 @@ from gdb_shared import *
 def filter_hook(packet_list):
     """Called through MRNet filters. Given a list of messages of lists of arecs."""
     msg_list = map(cPickle.loads, packet_list)
+    # Compute earliest sent time, if messages have them.
+    # Performance must be enabled globally, so only check first message.
+    new_time = None
+    if hasattr(msg_list[0], '_send_time'):
+        new_time = min(msg_list, key = lambda x: x._send_time)
     arec_list = map(lambda x: x.record, msg_list)
     new_list = arec_list.pop(0)
     for l in arec_list:
         new_list = combine_aggregation_lists(new_list, l)
     msg = GDBMessage(OUT_MSG, record = new_list)
+    if new_time:
+        msg._send_time = new_time
     return cPickle.dumps(msg, 0)
