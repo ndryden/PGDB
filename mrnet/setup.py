094ec21d1cfef749f6045653a954c614c21a1959
Nikoli Dryden <ndryden@xenosgame.com>
2013-11-25 12:51:13 -0600

    Added linking to boost libraries for MRNet module and filters.

--- mrnet/setup.py
+++ mrnet/setup.py
@@ -7,7 +7,7 @@ if use_mrnet400:
                       sources = ["mrnet_module.cpp"],
                       include_dirs = ["/collab/usr/global/tools/mrnet/chaos_5_x86_64_ib/mrnet-4.0.0/include"],
                       library_dirs = ["/collab/usr/global/tools/mrnet/chaos_5_x86_64_ib/mrnet-4.0.0/lib"],
-                      libraries = ["mrnet", "xplat"],
+                      libraries = ["mrnet", "xplat", "boost_timer-mt", "boost_system-mt"],
                       extra_compile_args = ["-O0", "-g"],
                       extra_link_args = ["-Wl,-rpath=/collab/usr/global/tools/mrnet/chaos_5_x86_64_ib/mrnet-4.0.0/lib", "-Wl,-E"])
 else:
