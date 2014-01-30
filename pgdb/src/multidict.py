94d9f8c09e43145c978b8e0d21ade13a8e9c7895
Nikoli Dryden <ndryden@xenosgame.com>
2014-01-30 15:42:27 -0600

    Removed multidict -- nothing uses it.

--- pgdb/src/multidict.py
+++
@@ -1,7 +0,0 @@
-"""A simple multidict using Python's built-in defaultdict class in combination with lists."""
-
-import collections.defaultdict
-
-def multidict():
-    """Return a new empty multidict."""
-    return collections.defaultdict(list)
