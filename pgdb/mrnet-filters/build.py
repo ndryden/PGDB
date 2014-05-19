ad4f1394a5f6e059c46096222918e4d1bd549b6d
Nikoli Dryden <ndryden@xenosgame.com>
2014-05-19 10:23:26 -0500

    Updated arec filter build system.

--- pgdb/mrnet-filters/build.py
+++ pgdb/mrnet-filters/build.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+"""Quick Python script to build the arec filter."""
+
 import os
-MRNetDir = "/home/ndryden"
-buildCommand = "g++ -Wall -Wextra -O0 -g -I"+MRNetDir+"/include -L"+MRNetDir+"/lib -Wl,-rpath="+MRNetDir+"/lib -fPIC -shared -rdynamic -o arec_filter.so arec_filter.cc -lmrnet -lxplat -lpython2.7 -lboost_timer-mt -lboost_system-mt"
-print buildCommand
-os.system(buildCommand);
+mrnet_path = "/home/ndryden/mrnet"
+build_command = "g++ -Wall -Wextra -O0 -g -I" + mrnet_dir + "/include -L" + mrnet_dir + "/lib -Wl,-rpath=" + mrnet_dir + "/lib -fPIC -shared -rdynamic -o arec_filter.so arec_filter.cc -lmrnet -lxplat -lpython2.7 -lboost_timer-mt -lboost_system-mt"
+print build_command
+os.system(build_command);
--- pgdb/mrnet-filters/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -Wall -Wextra -O0 -g -I/home/ndryden/include -L/home/ndryden/lib -Wl,-rpath=/home/ndryden/lib -fPIC -shared -rdynamic -o arec_filter.so arec_filter.cc -lmrnet -lxplat -lpython2.7 -lboost_timer-mt -lboost_system-mt
