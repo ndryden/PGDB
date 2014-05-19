2be5b37b8b4ca4bd6e37e7b1d03edc3b65e197aa
Nikoli Dryden <ndryden@xenosgame.com>
2014-05-19 10:35:11 -0500

    I should not commit changes before finishing my first cup of coffee. Fixed trailing text I should have removed.

--- pgdb/mrnet-filters/build.py
+++ pgdb/mrnet-filters/build.py
@@ -7,7 +7,7 @@ mrnet_path = "/home/ndryden/mrnet"
 python_lib = "python2.7"
 # Boost libraries that need to be used when building.
 boost_libs = ["boost_timer-mt", "boost_system-mt"]
-build_command = "g++ -Wall -Wextra -O0 -g -I" + mrnet_path + "/include -L" + mrnet_path + "/lib -Wl,-rpath=" + mrnet_path + "/lib -fPIC -shared -rdynamic -o arec_filter.so arec_filter.cc -lmrnet -lxplat" -lpython2.7 -lboost_timer-mt -lboost_system-mt
+build_command = "g++ -Wall -Wextra -O0 -g -I" + mrnet_path + "/include -L" + mrnet_path + "/lib -Wl,-rpath=" + mrnet_path + "/lib -fPIC -shared -rdynamic -o arec_filter.so arec_filter.cc -lmrnet -lxplat"
 build_command += " -l" + python_lib
 build_command += "".join([" -l" + lib for lib in boost_libs])
 print build_command
