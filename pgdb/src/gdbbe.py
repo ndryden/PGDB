bb7a8c307754c5922343c60580fcb5dec1dab06e
Nikoli Dryden <ndryden@xenosgame.com>
2014-07-02 18:42:05 -0500

    Updated some style and made changes to run_gdb_command.

--- pgdb/src/gdbbe.py
+++ pgdb/src/gdbbe.py
@@ -1,38 +1,47 @@
 """The back-end daemon invoked by LaunchMON.
 
-This handles initializing the back-end side of the LaunchMON and MRNet communication systems,
-deploying GDB, and sending commands and data back and forth.
+This handles initializing the back-end side of the LaunchMON and MRNet
+communication systems, deploying GDB, and sending commands and data back
+and forth.
 
 """
 
+from __future__ import print_function
 from conf import gdbconf
-from comm import *
+from comm import CommunicatorFE
 from gdb_shared import *
-from lmon.lmonbe import *
+from lmon.lmonbe import LMON_be
 import mi.gdbmi_parser as gdbparser
-from mi.gdbmi import *
+from mi.gdbmi import GDBMachineInterface
 from mi.varobj import VariableObject, VariableObjectManager
 from mi.commands import Command
-from mi.gdbmiarec import GDBMIAggregatedRecord, combine_records, combine_aggregated_records
+from mi.gdbmiarec import (GDBMIAggregatedRecord, combine_records,
+                          combine_aggregated_records)
 from mi.gdbmi_recordhandler import GDBMIRecordHandler
 from interval import Interval
 from varprint import VariablePrinter
 from sbd import SBDBE
-import signal, os, os.path, mmap, struct
+import signal
+import os
+import os.path
+import mmap
+import struct
+import time
 import posix_ipc
 
 class GDBBE:
     """The back-end GDB daemon process."""
 
     def init_gdb(self):
-        """Initialize GDB-related things, including launching the GDB process."""
+        """Initialize GDB-related things, and launch the GDB process."""
         # Indexed by MPI rank.
         self.varobjs = {}
         # Maps tokens to MPI rank.
         self.token_rank_map = {}
         self.record_handler = GDBMIRecordHandler()
-        self.record_handler.add_type_handler(self._watch_thread_created,
-                                             set([gdbparser.ASYNC_NOTIFY_THREAD_CREATED]))
+        self.record_handler.add_type_handler(
+            self._watch_thread_created,
+            set([gdbparser.ASYNC_NOTIFY_THREAD_CREATED]))
         gdb_env = {}
         if gdbconf.use_sbd:
             self.sbd = SBDBE(self.comm)
@@ -41,13 +50,14 @@ class GDBBE:
             self.sbd = None
 
         enable_pprint_cmd = Command("enable-pretty-printing")
-        enable_target_async_cmd = Command("gdb-set", args = ["target-async", "on"])
-        disable_pagination_cmd = Command("gdb-set", args = ["pagination", "off"])
-        enable_non_stop_cmd = Command("gdb-set", args = ["non-stop", "on"])
+        enable_target_async_cmd = Command("gdb-set",
+                                          args=["target-async", "on"])
+        disable_pagination_cmd = Command("gdb-set", args=["pagination", "off"])
+        enable_non_stop_cmd = Command("gdb-set", args=["non-stop", "on"])
         add_inferior_cmd = Command("add-inferior")
-        self.gdb = GDBMachineInterface(gdb = gdbconf.gdb_path,
-                                       gdb_args = ["-x", gdbconf.gdb_init_path],
-                                       env = gdb_env)
+        self.gdb = GDBMachineInterface(gdb=gdbconf.gdb_path,
+                                       gdb_args=["-x", gdbconf.gdb_init_path],
+                                       env=gdb_env)
         procs = self.comm.get_proctab()
         # Set up GDB.
         if not self.run_gdb_command(enable_pprint_cmd):
@@ -69,7 +79,7 @@ class GDBBE:
             self.rank_inferior_map[proc.mpirank] = 'i' + str(i)
             self.inferior_rank_map['i' + str(i)] = proc.mpirank
             i += 1
-            if not self.run_gdb_command(add_inferior_cmd, no_thread = True):
+            if not self.run_gdb_command(add_inferior_cmd, no_thread=True):
                 raise RuntimeError('Cound not add inferior i{0}!'.format(i - 1))
 
         # Maps MPI ranks to associated threads and vice-versa.
@@ -78,15 +88,19 @@ class GDBBE:
 
         if self.sbd:
             # Set up the list of executables for load file checking.
-            self.sbd.set_executable_names([os.path.basename(proc.pd.executable_name) for proc in procs])
+            self.sbd.set_executable_names(
+                [os.path.basename(proc.pd.executable_name) for proc in procs])
 
         # Attach processes.
         for proc in procs:
-            if not self.run_gdb_command(Command("target-attach",
-                                                opts = {'--thread-group': self.rank_inferior_map[proc.mpirank]},
-                                                args = [proc.pd.pid]),
-                                        proc.mpirank, no_thread = True):
-                raise RuntimeError("Could not attach to rank {0}!".format(proc.mpirank))
+            if not self.run_gdb_command(
+                    Command("target-attach",
+                            opts={'--thread-group':
+                                  self.rank_inferior_map[proc.mpirank]},
+                            args=[proc.pd.pid]),
+                    proc.mpirank, no_thread=True):
+                raise RuntimeError("Could not attach to rank {0}!".format(
+                    proc.mpirank))
             self.varobjs[proc.mpirank] = VariableObjectManager()
             # Cludge to fix GDB not outputting records for the i1 attach.
             if self.rank_inferior_map[proc.mpirank] == 'i1':
@@ -114,29 +128,23 @@ class GDBBE:
         for proc in self.proctab:
             os.kill(proc.pd.pid, signal.SIGTERM)
 
-    def run_gdb_command(self, command, ranks = None, no_thread = False):
+    def run_gdb_command(self, command, ranks=None, no_thread=False):
         """Run a GDB command.
 
         command is a Command object representing the command.
         ranks is an Interval of the ranks to run the command on.
-        If ranks is None, run on the current GDB inferior.
+        If ranks is None, run on the current inferior.
+        If no_thread is True, this does not specify a particular thread.
 
-        Returns a dictionary, indexed by ranks, of the tokens used.
-        If running on the current inferior, the "rank" is -1.
+        Returns True on success, False on error.
 
         """
         if isinstance(ranks, int):
             # Special case for a single int.
             # Toss it in a list; don't need a full Interval.
             ranks = [ranks]
-        tokens = {}
         if ranks is None:
-            # Send to the current inferior.
-            if not self.gdb.send(command):
-                print "GDB error, exiting."
-                self.quit = True
-                return
-            tokens[-1] = command.token
+            return self.gdb.send(command)
         else:
             for rank in ranks:
                 if rank in self.rank_inferior_map:
@@ -144,14 +152,11 @@ class GDBBE:
                     if (not no_thread and
                         rank in self.rank_thread_map and
                         command.get_opt('--thread') is None):
-                        command.add_opt('--thread', self.rank_thread_map[rank][0])
+                        command.add_opt('--thread',
+                                        self.rank_thread_map[rank][0])
                     if not self.gdb.send(command):
-                        print "GDB error, exiting."
-                        self.quit = True
-                        return
-                    tokens[rank] = command.token
-                    self.token_rank_map[command.token] = rank
-        return tokens
+                        return False
+        return True
 
     def init_handlers(self):
         """Initialize message handlers used on data we receive over MRNet."""
@@ -211,7 +216,8 @@ class GDBBE:
         # Exception guard if we have an error before comm init.
         try:
             self.shutdown()
-        except AttributeError: pass
+        except AttributeError:
+            pass
 
     def die_handler(self, msg):
         """Handle a die message by exiting."""
@@ -233,7 +239,7 @@ class GDBBE:
             ranks = msg.ranks
         if not self.run_gdb_command(msg.command, ranks):
             # TODO: Send die message.
-            print "Managed to get a bad command '{0}'.".format(msg.command)
+            print("Managed to get a bad command '{0}'.".format(msg.command))
 
     def kill_handler(self, msg):
         """Handle a kill message, killing all processes."""
@@ -248,7 +254,7 @@ class GDBBE:
         self.filters.difference_update(msg.filter_types)
 
     def varprint_handler(self, msg):
-        """Handle the varprint message and begin sequence. See VariablePrinter."""
+        """Handle the varprint message and begin sequence."""
         self.variable_printer.varprint_handler(msg)
 
     def is_filterable(self, record):
@@ -263,13 +269,14 @@ class GDBBE:
         if self.sbd:
             self.sbd.file_data_handler(msg)
         else:
-            print "Got SBD file data when SBD is not enabled"
+            print("Got SBD file data when SBD is not enabled")
 
     def main(self):
         """Main send/receive loop.
 
-        This receives data on MRNet (non-blocking), processes the messages, and then sends any
-        data that was read from GDB. This then sleeps for a short while to avoid heavy CPU use.
+        This receives data on MRNet (non-blocking), processes the messages,
+        and then sends any data that was read from GDB. This then sleeps for a
+        short while to avoid heavy CPU use.
 
         """
         while True:
@@ -279,18 +286,20 @@ class GDBBE:
             if self.sbd:
                 # Check for data from the GDB process for LOAD_FILE.
                 self.sbd.sbd_check()
-            msg = self.comm.recv(blocking = False)
+            msg = self.comm.recv(blocking=False)
             if msg is not None:
                 # Received data.
                 if msg.msg_type in self.msg_handlers:
                     self.msg_handlers[msg.msg_type](msg)
                 else:
-                    print "Got a message {0} with no handler.".format(msg.msg_type)
+                    print("Got a message {0} with no handler.".format(
+                        msg.msg_type))
 
             records = []
             ranks = []
             for record in self.gdb.read():
-                if record.record_subtypes == set(["stopped"]) and self.doing_startup:
+                if (record.record_subtypes == set(["stopped"]) and
+                    self.doing_startup):
                     self.startup_done_count += 1
                     if self.startup_done_count == self.comm.get_proctab_size():
                         self.doing_startup = False
@@ -304,14 +313,19 @@ class GDBBE:
             if records:
                 arecs = combine_records(records, ranks)
                 if self.doing_startup:
-                    self.startup_arec = combine_aggregated_records(self.startup_arecs + arecs)
+                    self.startup_arec = combine_aggregated_records(
+                        self.startup_arecs + arecs)
                 else:
-                    if self.startup_done_count == self.comm.get_proctab_size() and self.startup_arecs is not None:
-                        arecs = combine_aggregated_records(self.startup_arecs + arecs)
-                        self.comm.send(GDBMessage(OUT_MSG, record = arecs), self.comm.frontend)
+                    if (self.startup_done_count == self.comm.get_proctab_size()
+                        and self.startup_arecs is not None):
+                        arecs = combine_aggregated_records(
+                            self.startup_arecs + arecs)
+                        self.comm.send(GDBMessage(OUT_MSG, record=arecs),
+                                       self.comm.frontend)
                         self.startup_arecs = None
                     else:
-                        self.comm.send(GDBMessage(OUT_MSG, record = arecs), self.comm.frontend)
+                        self.comm.send(GDBMessage(OUT_MSG, record=arecs),
+                                       self.comm.frontend)
 
             # Sleep a bit to reduce banging on the CPU.
             time.sleep(0.01)
