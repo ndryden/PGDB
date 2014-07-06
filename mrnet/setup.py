fcc55a42419944c50d51e69b53cffcb7b3e6caba
Nikoli Dryden <ndryden@xenosgame.com>
2014-07-06 15:49:47 -0500

    Cleaned up setup.pys.

--- mrnet/setup.py
+++ mrnet/setup.py
@@ -1,13 +1,18 @@
+"""Build and install MRNet."""
+
 from distutils.core import setup, Extension
 
 mrnet_path = raw_input("Please type the base path to MRNet: ")
 if mrnet_path[-1:] == '/':
     mrnet_path = mrnet_path[:-1]
 mrnet = Extension("MRNet",
-                  sources = ["mrnet_module.cpp"],
-                  include_dirs = [mrnet_path + "/include"],
-                  library_dirs = [mrnet_path + "/lib"],
-                  libraries = ["mrnet", "xplat", "boost_timer-mt", "boost_system-mt"],
-                  extra_compile_args = [],
-                  extra_link_args = ["-Wl,-rpath=" + mrnet_path + "/lib", "-Wl,-E"])
-setup(name = "MRNet", version = "0.01", description = "Python interface to MRNet", ext_modules = [mrnet])
+                  sources=["mrnet_module.cpp"],
+                  include_dirs=[mrnet_path + "/include"],
+                  library_dirs=[mrnet_path + "/lib"],
+                  libraries=["mrnet", "xplat",
+                             "boost_timer-mt", "boost_system-mt"],
+                  extra_compile_args=[],
+                  extra_link_args=["-Wl,-rpath=" + mrnet_path + "/lib",
+                                   "-Wl,-E"])
+setup(name="MRNet", version="0.01",
+      description="Python interface to MRNet", ext_modules=[mrnet])
