bc1a8aa68edf58953058123e4cb2e04765f8c486
Nikoli Dryden <ndryden@xenosgame.com>
2015-11-03 02:23:58 -0600

    Reorganized the structure a bit. Removed LaunchMON bindings (now part of a separate repo). Removed unneeded files.

--- lmon/setup.py
+++
@@ -1,8 +0,0 @@
-"""Setup script for installing LaunchMON."""
-
-from distutils.core import setup
-
-setup(name="lmon",
-      version="0.01",
-      description="Python interface to LaunchMON",
-      packages=["lmon"])
